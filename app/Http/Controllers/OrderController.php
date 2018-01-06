bf253f02209a9c277b85f3fa6bc7fa0366f9f6db
sonquanghens <sonquanghens@gmail.com>
2018-01-06 15:29:15 +0700

    create_twilo

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -75,18 +75,18 @@ class OrderController extends Controller
       $date_end = $request->Input('date_end');
       $order = Input::get ( 'search_order' );
       $orderUpercase = Str::lower($order);
-      if(empty($date_start) && empty($date_end))
-      {
-        //there are not date_end and date_start , but there is Name_search
+      $note = Input::get ( 'note' );
+    // \Twilio::message('+84' . Auth::user()->phone_number,'ban vua dat hang giay xong ' );
+      if (!empty($note)) {
         $orders = Order::join('users', 'orders.user_id', '=', 'users.id')
-                            ->where('note','LIKE','%'.$order.'%')
-                            ->orwhere('users.name','LIKE','%'.$order.'%')
-                            ->orWhere('note','LIKE','%'.$orderUpercase.'%')
+                            ->where('users.name','LIKE','%'.$order.'%')
+                            ->where('note','LIKE','%'.$note.'%')
+                            ->where('date_order', '>=', $date_start )
+                            ->where('date_order', '<=', $date_end )
                             ->paginate(10);
         $sum_orders = Order::join('users', 'orders.user_id', '=', 'users.id')
-                            ->where('note','LIKE','%'.$order.'%')
-                            ->orwhere('users.name','LIKE','%'.$order.'%')
-                            ->orWhere('note','LIKE','%'.$orderUpercase.'%')
+                            ->where('users.name','LIKE','%'.$order.'%')
+                            ->where('note','LIKE','%'.$note.'%')
                             ->get();
                             // dd($sum_orders);
 
@@ -96,33 +96,73 @@ class OrderController extends Controller
           }
             return view('auth.admin.order.list_order',compact('orders','sum_total' ));
       }
-      elseif (empty($date_end)) {
-        //there are not date_end but there is date_start
-            $orders = Order::join('users', 'orders.user_id', '=', 'users.id')
-            ->where('users.name','LIKE','%'.$order.'%')
-            ->where('date_order', '>=', $date_start )
+      else {
+        if(empty($date_start) && empty($date_end))
+        {
+          //there are not date_end and date_start , but there is Name_search
+          $orders = Order::join('users', 'orders.user_id', '=', 'users.id')
+                              ->where('note','LIKE','%'.$order.'%')
+                              ->orwhere('users.name','LIKE','%'.$order.'%')
+                              ->orWhere('note','LIKE','%'.$orderUpercase.'%')
+                              ->paginate(10);
+          $sum_orders = Order::join('users', 'orders.user_id', '=', 'users.id')
+                              ->where('note','LIKE','%'.$order.'%')
+                              ->orwhere('users.name','LIKE','%'.$order.'%')
+                              ->orWhere('note','LIKE','%'.$orderUpercase.'%')
+                              ->get();
+                              // dd($sum_orders);
+
+               $sum_total =0;
+            foreach ($sum_orders as $key => $order) {
+                $sum_total = $sum_total + $order->total;
+            }
+              return view('auth.admin.order.list_order',compact('orders','sum_total' ));
+        }
+        elseif (empty($date_end)) {
+          //there are not date_end but there is date_start
+              $orders = Order::join('users', 'orders.user_id', '=', 'users.id')
+              ->where('users.name','LIKE','%'.$order.'%')
+              ->where('date_order', '>=', $date_start )
+              ->orderBy('date_order','asc')
+              ->paginate(10);
+
+              // sum total
+              $sum_orders = Order::join('users', 'orders.user_id', '=', 'users.id')
+              ->where('users.name','LIKE','%'.$order.'%')
+              ->where('date_order', '>=', $date_start )
+              ->orderBy('date_order','asc')
+              ->get();
+              $sum_total =0;
+              foreach ($sum_orders as $key => $order) {
+                  $sum_total = $sum_total + $order->total;
+              }
+                return view('auth.admin.order.list_order',compact('orders','sum_total'));
+        }
+        elseif (empty($date_start)) {
+          //there is date_end no date_end and no Name_search
+            $orders = Order::where('date_order', '<=', $date_end)
             ->orderBy('date_order','asc')
             ->paginate(10);
-
-            // sum total
-            $sum_orders = Order::join('users', 'orders.user_id', '=', 'users.id')
-            ->where('users.name','LIKE','%'.$order.'%')
-            ->where('date_order', '>=', $date_start )
+            //sum Total
+            $sum_orders = Order::where('date_order', '<=', $date_end)
             ->orderBy('date_order','asc')
             ->get();
             $sum_total =0;
             foreach ($sum_orders as $key => $order) {
                 $sum_total = $sum_total + $order->total;
             }
-              return view('auth.admin.order.list_order',compact('orders','sum_total'));
-      }
-      elseif (empty($date_start)) {
-        //there is date_end no date_end and no Name_search
-          $orders = Order::where('date_order', '<=', $date_end)
+            return view('auth.admin.order.list_order',compact('orders','sum_total'));
+        }
+        else {
+          // there are date_end and date_start no Name_search
+          $orders = Order::where('date_order', '>=', $date_start)
+          ->where('date_order', '<=', $date_end)
           ->orderBy('date_order','asc')
           ->paginate(10);
-          //sum Total
-          $sum_orders = Order::where('date_order', '<=', $date_end)
+
+          //total sum
+          $sum_orders = Order::where('date_order', '>=', $date_start)
+          ->where('date_order', '<=', $date_end)
           ->orderBy('date_order','asc')
           ->get();
           $sum_total =0;
@@ -130,24 +170,7 @@ class OrderController extends Controller
               $sum_total = $sum_total + $order->total;
           }
           return view('auth.admin.order.list_order',compact('orders','sum_total'));
-      }
-      else {
-        // there are date_end and date_start no Name_search
-        $orders = Order::where('date_order', '>=', $date_start)
-        ->where('date_order', '<=', $date_end)
-        ->orderBy('date_order','asc')
-        ->paginate(10);
-
-        //total sum
-        $sum_orders = Order::where('date_order', '>=', $date_start)
-        ->where('date_order', '<=', $date_end)
-        ->orderBy('date_order','asc')
-        ->get();
-        $sum_total =0;
-        foreach ($sum_orders as $key => $order) {
-            $sum_total = $sum_total + $order->total;
         }
-        return view('auth.admin.order.list_order',compact('orders','sum_total'));
       }
 
     }
--- resources/views/auth/admin/order/list_order.blade.php
+++ resources/views/auth/admin/order/list_order.blade.php
@@ -12,8 +12,15 @@
             <div class="input-group" style="float: right;    margin-top: 19px;">
               <!-- //form seach -->
               <form class="navbar-form navbar-left" action="/admin/search_order" role="search" method="GET" >
-                <a href="{{url('/admin/order/done')}}"><button type="button"  class="btn-info btn-sm">Done</button></a>
-                <a href="{{url('/admin/order/in')}}"><button type="button" class="btn-info btn-sm" name="note">in process</button></a>
+                {!!Form::label('Note', null, ['class' => 'form-control'])!!}
+                {!! Form::select('note',
+                  [
+                     ''      => '',
+                     'Done'        => 'Done',
+                     'in process'        => 'in process',
+                 ]
+                 , null, [ 'class' =>  'form-control',])
+                !!}
                 {!!Form::label('From', null, ['class' => 'form-control'])!!}
                 {!! Form::date('date_start', null, ['class' => 'form-control']) !!}
                 {!!Form::label('to', null, ['class' => 'form-control'])!!}
